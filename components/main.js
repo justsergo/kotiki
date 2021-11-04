16a016085969657b4158429c9eb0a1a6d072f28a
sergey <sergeydanilov2019@gmail.com>
2021-11-04 19:31:24 +0300

    startap

---
+++ components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+export default App;
